1c80da0cff74f1eb04d477332cdee603f03134c8
Sonam Naroliya <sonam.naroliya@magicworkshop.ai>
2026-08-10 13:28:30 +0530

    Date-09.Aug.2026 Shopping list, Following tab, Create Recipe, added new test cases

---
+++ src/test/java/tests/VerifyFollowingTab.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import driver.DriverFactory;
+import pagesObjects.Profile.Following_Tab;
+
+import utils.Scroll;
+
+public class VerifyFollowingTab extends BaseTest {
+
+    @Test
+    public void verifyFollowingTabDetails()  throws Exception {
+       
+         Following_Tab following =
+                new Following_Tab(
+                        DriverFactory.getDriver());
+// 
+        Scroll scroll = new Scroll( DriverFactory.getDriver());
+
+        following.Click_Profile_Tab();
+
+        Thread.sleep(2000);
+
+        following.Click_Following_Tab();
+
+        Thread.sleep(2000);
+
+        following.Click_Follow_Button();
+
+        Thread.sleep(2000);
+
+        following.Verify_Following_count();
+
+        Thread.sleep(2000);
+
+        following.Open_Profile();
+
+         }
+        }
